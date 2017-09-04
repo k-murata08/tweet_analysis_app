771c10378af6449f44e51b11250bf337f02b9b46
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-04 16:42:15 +0900

    Edit role

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+from tweet_analysis import const as tc
+
 
 class Profile(models.Model):
-    role = models.IntegerField(blank=False)
+    role = models.IntegerField(blank=False, choices=tc.USER_ROLES)
     user = models.OneToOneField(User)
 
 
---
+++ tweet_analysis/const.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+USER_ROLES = (
+    (1, 'admin'),
+    (2, 'general')
+)
