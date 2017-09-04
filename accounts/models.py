98a7f8eceb5bc7c8105130e9b58d1121707b7030
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-04 16:46:39 +0900

    Set maxlength

--- accounts/models.py
+++ accounts/models.py
@@ -9,12 +9,12 @@ from tweet_analysis import const as tc
 
 class Profile(models.Model):
     ROLES = tc.USER_ROLES
-    
+
     role = models.IntegerField(blank=False, choices=ROLES)
     user = models.OneToOneField(User)
 
 
 class TwitterAccount(models.Model):
-    twitter_id = models.CharField(blank=False)
-    username = models.CharField(blank=False)
+    twitter_id = models.CharField(blank=False, max_length=30)
+    username = models.CharField(blank=False, max_length=30)
     image_url = models.URLField(blank=False)
