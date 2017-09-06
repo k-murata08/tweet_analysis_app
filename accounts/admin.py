3162432a18ece58ccfbfaaf4b51c1208fcad6048
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-06 11:41:22 +0900

    Update admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,11 +7,11 @@ from accounts.models import OathKey, TwitterAccount
 
 
 class OathKeyAdmin(admin.ModelAdmin):
-    list_display = ('consumer_key', 'consumer_secret', 'access_token', 'access_token_secret', 'twitter_id')
+    list_display = ('consumer_key', 'consumer_secret', 'access_token', 'access_token_secret', 'twitter_id', 'created_at')
 
 
 class TwitterAccountAdmin(admin.ModelAdmin):
-    list_display = ('twitter_id', 'username', 'image_url')
+    list_display = ('twitter_id', 'username', 'image_url', 'created_at')
 
 
 admin.site.register(OathKey, OathKeyAdmin)
