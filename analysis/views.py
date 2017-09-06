f75c41f959e32734e788e13ff5c679db9cd912d9
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-06 11:35:11 +0900

    Delete import

--- analysis/views.py
+++ analysis/views.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
-from accounts.models import TwitterAccount
 import utils as analysis_utils
 
 
