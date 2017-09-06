11f2b408e829df60d62d58d92ab79e74d9ed0630
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-06 14:09:04 +0900

    Update index

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
-from accounts.models import TwitterAccount
+from accounts.models import TwitterAccount, OathKey
 
 
 def redirect_index(request):
     accounts = TwitterAccount.objects.all()
+    oath_key = OathKey.get_oath()
 
     return render(request, 'index.html', {
-        'accounts': accounts
+        'accounts': accounts,
+        'oath_key': oath_key
     })
