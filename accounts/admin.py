016b9cecf15bff8c4af0cac733255096041f8b85
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-06 11:32:44 +0900

    Update account admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import OathKey, TwitterAccount
+
+
+class OathKeyAdmin(admin.ModelAdmin):
+    list_display = ('consumer_key', 'consumer_secret', 'access_token', 'access_token_secret', 'twitter_id')
+
+
+class TwitterAccountAdmin(admin.ModelAdmin):
+    list_display = ('twitter_id', 'username', 'image_url')
+
+
+admin.site.register(OathKey, OathKeyAdmin)
+admin.site.register(TwitterAccount, TwitterAccountAdmin)
+
