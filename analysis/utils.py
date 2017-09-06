dbe6fb81447b9c0576ed5391d0717f34dbb1c1f8
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-06 16:45:32 +0900

    Fix index

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from accounts.models import TwitterAccount, OathKey
 
@@ -7,8 +8,10 @@ from accounts.models import TwitterAccount, OathKey
 def redirect_index(request):
     accounts = TwitterAccount.objects.all()
     oath_key = OathKey.get_oath()
+    users = User.objects.all()
 
     return render(request, 'index.html', {
         'accounts': accounts,
-        'oath_key': oath_key
+        'oath_key': oath_key,
+        'users': users,
     })
