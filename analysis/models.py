ba72da3ed90f1c7dd9b899f8590c486d5c107cb2
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-08 17:20:02 +0900

    Create analysis model

--- analysis/models.py
+++ analysis/models.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Analysis(models.Model):
+    account = models.ForeignKey('accounts.TwitterAccount')
+    user = models.ForeignKey(User)
+    category = models.IntegerField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CommonFollowRecord(models.Model):
+    twitter_id = models.CharField(blank=False, max_length=30)
+    username = models.CharField(blank=False, max_length=30)
+    common_count = models.PositiveIntegerField(blank=False)
+    followers_count = models.PositiveIntegerField(blank=False)
+    follow_rate = models.FloatField(blank=False)
+    follow_ratio = models.FloatField(blank=False)
+    factor = models.FloatField(blank=False)
+    analysis = models.ForeignKey('Analysis')
--- tweet_analysis/const.py
+++ tweet_analysis/const.py
@@ -4,3 +4,9 @@ USER_ROLES = (
     (1, 'admin'),
     (2, 'general')
 )
+
+ANALYSIS_CATEGORY = {
+    'common_follow': 1,
+    'common_fav': 2,
+    'common_rt': 3
+}
