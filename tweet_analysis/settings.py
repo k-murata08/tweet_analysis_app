f5ba2f9b2db3e57bc8f347116c8e3033c729d246
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-08 15:45:33 +0900

    mysqlの設定

--- tweet_analysis/settings.py
+++ tweet_analysis/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'tweet_analysis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tweet_analysis',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
