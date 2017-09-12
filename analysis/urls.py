fa0e8b761ac8cf859a628cf775e002a9b514bd88
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-12 16:42:58 +0900

    ファボ分析結果追加

--- analysis/urls.py
+++ analysis/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^common_follow/result/$', views.common_follow_result, name='common_follow_result'),
     url(r'^common_follow/result/(?P<pk>[0-9]+)/$', views.common_follow_result_detail, name='common_follow_result_detail'),
     url(r'^common_fav/$', views.common_fav_form, name='common_fav_form'),
+    url(r'^common_fav/result/$', views.common_fav_result, name='common_fav_result'),
+    url(r'^common_fav/result/(?P<pk>[0-9]+)/$', views.common_fav_result_detail, name='common_fav_result_detail'),
     url(r'^common_rt/$', views.common_rt_form, name='common_rt_form'),
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 import utils as analysis_utils
 from .forms import AnalysisAccountForm
 from accounts.models import TwitterAccount
-from .models import Analysis, CommonFollowRecord
+from .models import Analysis, CommonFollowRecord, CommonFavoriteRecord
 import tweet_analysis.const as ta_const
 
 
@@ -75,7 +75,8 @@ def common_follow_result(request):
 
     return render(request, 'result.html', {
         'analysis': analysis,
-        'title': "共通フォロー分析"
+        'title': "共通フォロー分析",
+        'category': ta_const.ANALYSIS_CATEGORY
     })
 
 
@@ -83,6 +84,27 @@ def common_follow_result_detail(request, pk):
     analysis = Analysis.objects.get(id=pk)
     records = CommonFollowRecord.objects.filter(analysis=analysis).order_by('-common_count')
     return render(request, 'common_follow_result_detail.html', {
+        'records': records,
+        'analysis': analysis
+    })
+
+
+def common_fav_result(request):
+    analysis = Analysis.objects.filter(
+        category=ta_const.ANALYSIS_CATEGORY['common_fav']
+    ).order_by('-created_at')
+
+    return render(request, 'result.html', {
+        'analysis': analysis,
+        'title': '共通ファボ分析',
+        'category': ta_const.ANALYSIS_CATEGORY
+    })
+
+
+def common_fav_result_detail(request, pk):
+    analysis = Analysis.objects.get(id=pk)
+    records = CommonFavoriteRecord.objects.filter(analysis=analysis).order_by('-common_count')
+    return render(request,'common_fav_result_detail.html', {
         'records': records,
         'analysis': analysis
     })
