b05aeb0019bd0f61e05ae8eb1f772ba868f310f7
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-13 13:59:56 +0900

    Setting update

--- tweet_analysis/wsgi.py
+++ tweet_analysis/wsgi.py
@@ -3,6 +3,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweet_analysis.settings")
 
 application = Cling(get_wsgi_application())
