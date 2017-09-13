490465bff553af9fb70c4e93405e7bb25257555c
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-13 17:03:01 +0900

    Update setting

--- tweet_analysis/settings.py
+++ tweet_analysis/settings.py
@@ -22,9 +22,6 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
