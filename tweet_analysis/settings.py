baba8d212816ba590a758e02c36d1639571f37c0
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-13 16:59:10 +0900

    Update settings

--- tweet_analysis/settings.py
+++ tweet_analysis/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tweet_analysis.urls'
