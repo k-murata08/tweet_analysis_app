db48919e6601533eb3de6c4042a4fff534024316
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-09-13 12:42:35 +0900

    Heroku setting

--- tweet_analysis/settings.py
+++ tweet_analysis/settings.py
@@ -130,7 +130,22 @@ USE_L10N = True
 USE_TZ = False
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- tweet_analysis/wsgi.py
+++ tweet_analysis/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for tweet_analysis project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweet_analysis.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
